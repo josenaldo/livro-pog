6afcf16ae2657ec4e970f11d592cba995d96dd6c
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-08-26 13:22:35 -0300

    Formatação do link

--- src/components/elements/Logo.js
+++ src/components/elements/Logo.js
@@ -1,16 +1,29 @@
 import Image from 'next/image'
-import { Box } from '@mui/material'
+import Link from 'next/link'
+import { Box, Typography } from '@mui/material'
 
 const Logo = () => {
     return (
-        <Box component="header">
-            <Image
-                src="/images/logo/brand.png"
-                alt="Logo do livro Programação Orientada a Gambiarra"
-                width="48px"
-                height="48px"
-            />
-        </Box>
+        <Link href="/" passHref>
+            <Box
+                component="a"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    textDecoration: 'none',
+                    color: 'inherit',
+                }}
+            >
+                <Image
+                    src="/images/logo/brand.png"
+                    alt="Logo do livro Programação Orientada a Gambiarra"
+                    width="48px"
+                    height="48px"
+                />
+                <Typography fontWeight="bold">POG</Typography>
+            </Box>
+        </Link>
     )
 }
 
